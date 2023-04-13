6ace16d6b5384cf5d4aa995d38709dba4d1d2596
SteveDoyle2 <mesheb82@gmail.com>
2023-04-12 21:55:55 -0700

    printing line numbers

--- rustran.rs
+++ rustran.rs
@@ -1,3 +1,6 @@
+// Rust Tutorial 101
+// mut: flags a variable as mutable
+//----------------------------------------------
 // This is a comment, and is ignored by the compiler.
 // You can test this code by clicking the "Run" button over there ->
 // or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
@@ -8,16 +11,40 @@
 
 // This is the main function.
 
-use std::io::Read;
+//use std::io::Read;
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader };
+
+
+fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
+    // Open the file in read-only mode.
+   //let mut file = std::fs::File::open(filename).unwrap();
+
+   //let mut contents = String::new();
+   //file.read_to_string(&mut contents).unwrap();
+   //print!("{}", contents);
+   
+   //------------------------
+   let file = File::open(filename).unwrap();
+   // TODO: read includes
+   // TODO: split into system, executive, case, bulk lines
+
+
+   // Read the file line by line, and return an iterator of the lines of the file.
+   return io::BufReader::new(file).lines();
+}
+
 
 fn main() {
     // Statements here are executed when the compiled binary is called.
-
+    let lines = read_lines("./rod.bdf".to_string());
+    println!("back to code");
+    let mut i = 0;
+    for line in lines {
+        println!("{}: {}", i, line.unwrap());
+        i += 1;
+    }
+    
     // Print text to the console.
     println!("Hello World!");
-
-   let mut file = std::fs::File::open("rod.bdf").unwrap();
-   let mut contents = String::new();
-   file.read_to_string(&mut contents).unwrap();
-   print!("{}", contents);
 }
